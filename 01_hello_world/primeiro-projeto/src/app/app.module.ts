98af8bb30bea26c8f0db01ef361f5296533d8b3c
Julio Pinheiro <juliorenan@live.com>
2020-12-26 15:16:49 -0300

    feat: #05 Módulos(ngModule)

--- 01_hello_world/primeiro-projeto/src/app/app.module.ts
+++ 01_hello_world/primeiro-projeto/src/app/app.module.ts
@@ -4,7 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
+
+// Decorator, NgModule.
+// Metadado Declarations: Listamos todas as diretivas, pipes e componentes que iremos utilizar nesse módulo.
+// Metadado Imports: Outros módulos que gostariamos de utilizar neste modulo. Ou dentro de algum componente que faz parte deste modulo.
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +17,12 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Onde colocamos os serviços que ficarão disponíveis para todos os componentes do módulo
+  bootstrap: [AppComponent] // Componente que é iniciado.
 })
+
+// Declaração da Classe
 export class AppModule { }
