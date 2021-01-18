ba61310e5193d34346a007467970605aaa155ac0
Julio Pinheiro <juliorenan@live.com>
2021-01-18 19:36:49 -0300

    #53 Rotas: definindo e extraindo parametros

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursosComponent } from "./cursos/cursos.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
@@ -14,6 +15,10 @@ export const APP_ROUTES: Routes = [
     },
     {
         path: 'cursos', component: CursosComponent
+    },
+
+    {
+        path: 'cursos/:id', component: CursoDetalheComponent
     },
 ];
 
