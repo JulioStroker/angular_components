e962e9cb01da9256c260c7d394967a7a7516859b
Julio Pinheiro <juliorenan@live.com>
2021-01-18 17:58:44 -0300

    #50 - Criado componentes para exemplo de rotas

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CursosComponent } from "./cursos/cursos.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+
+
+export const APP_ROUTES: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'cursos', component: CursosComponent
+    },
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
+
+
